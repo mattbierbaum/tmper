880d33dba5044e680d582c4fab389ad53b0d37ab
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-10-28 17:07:33 -0400

    add upload, download utility to same file; python3 compatibility

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 setup(name='tmpr',
       license='MIT License',
       author='Matt Bierbaum',
-      version='0.0.1',
+      version='0.0.2',
 
       install_requires=["tornado>=4.3"],
-      scripts=['tmpr.py']
+      scripts=['tmpr']
 )
