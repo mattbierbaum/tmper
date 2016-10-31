b3ac674da00d4c763240820811fdf7aa1670d02a
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-10-31 14:15:13 -0400

    moving version once more, only reading readme

--- setup.py
+++ setup.py
@@ -6,22 +6,22 @@ from distutils.core import setup
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
-def version():
-    return re.findall(r".*__version__ = \'(.*)\'.*", read('tmpr'))[0]
-
-def readme():
-    return read('README.md')
+try:
+    readme = read('README.md')
+except IOError as e:
+    readme = ''
 
 setup(name='tmpr',
       license='MIT License',
       author='Matt Bierbaum',
       url='https://github.com/mattbierbaum/tmpr',
-      version=version(),
+      version='0.4.2',
 
       install_requires=["tornado>=4.3"],
       scripts=['tmpr'],
+      package_data={'': ['README.md']},
 
       platforms='osx, posix, linux, windows',
       description='Temporary file sharing using simple two digit codes.',
-      long_description=readme(),
+      long_description=readme,
 )
