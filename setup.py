98bb5ea731ef08bee2b713155a69ffab358c5b45
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-10-31 11:55:00 -0400

    version, comments, extra checks for user input

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #from setuptools import setup
+import re
 from distutils.core import setup
+
+# exctract version from the main python file
+version = re.findall(r".*__version__ = \'(.*)\'.*", open('./tmpr').read())[0]
 
 setup(name='tmpr',
       license='MIT License',
       author='Matt Bierbaum',
-      version='0.2a',
+      version=version,
 
       install_requires=["tornado>=4.3"],
       scripts=['tmpr']
