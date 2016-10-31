d743cb0ceb13a00d8dd3ce1e4dafaaa6788e3a0a
Matt Bierbaum <matt.bierbaum@gmail.com>
2016-10-31 13:25:25 -0400

    adding more info to PKG_INFO

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 #from setuptools import setup
+import os
 import re
 from distutils.core import setup
 
-# exctract version from the main python file
-version = re.findall(r".*__version__ = \'(.*)\'.*", open('./tmpr').read())[0]
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+def version():
+    return re.findall(r".*__version__ = \'(.*)\'.*", read('tmpr'))[0]
+
+def readme():
+    return read('README.md')
 
 setup(name='tmpr',
       license='MIT License',
       author='Matt Bierbaum',
-      version=version,
+      version=version(),
 
       install_requires=["tornado>=4.3"],
-      scripts=['tmpr']
+      scripts=['tmpr'],
+
+      summary='Temporary file sharing using simple two digit codes.',
+      long_description=readme(),
 )
